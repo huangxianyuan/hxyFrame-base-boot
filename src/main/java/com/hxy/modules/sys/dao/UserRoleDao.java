82d0df035dde378e77e90116ae0810f85d9c9fcc
huangxianyuan <15208283385@163.com>
2017-11-28 18:19:08 +0800

    修改数据权限管理

--- src/main/java/com/hxy/modules/sys/dao/UserRoleDao.java
+++ src/main/java/com/hxy/modules/sys/dao/UserRoleDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * 用户角色关系表
  * 
- * @author chenshun
+ * @author hxy
  * @email huangxianyuan@gmail.com
  * @date 2017-05-03 10:07:59
  */
--- src/main/java/com/hxy/modules/sys/entity/UserEntity.java
+++ src/main/java/com/hxy/modules/sys/entity/UserEntity.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * 系统用户表
  * 
- * @author chenshun
+ * @author hxy
  * @email huangxianyuan@gmail.com
  * @date 2017-05-03 09:41:38
  */
